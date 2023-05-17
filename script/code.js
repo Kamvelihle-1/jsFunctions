92c119e76d8382269c4fb8ff927c897b85e369ab
SmasherK <ksikalan@gmail.com>
2023-05-17 10:06:08 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    console.log("Hello "+ name);
+}
+greet("Karl")
+function multiply(num1,num2) {
+    console.log(num1*num2);
+}
+multiply(20,34)
+function tableOfFive(num) {
+    let multFive = num*5
+    console.log(multFive);
+}
+tableOfFive(15)
